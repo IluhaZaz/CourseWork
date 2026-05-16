e1c4f3a7ee9ce55ac24e3f82ba1b5a927df01621
Ilya Zazvonov <izazvonov@rvision.ru>
2026-05-16 17:00:56 +0400

    fix

--- back/src/main.py
+++ back/src/main.py
@@ -25,7 +25,7 @@ app = FastAPI(title="YourToDoList")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["http://localhost:4200", "http://localhost:8080"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
